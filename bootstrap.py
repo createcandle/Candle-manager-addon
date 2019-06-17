f7b84a529882ef71168a7ec9ab5ce8322596124c
Create Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2019-06-17 19:09:42 +0200

    0.0.1

--- bootstrap.py
+++ bootstrap.py
@@ -42,8 +42,12 @@ def install_packages():
 
 try:
     sys.path.append(os.path.join(_BASE_DIR, 'lib'))
+except:
+    pass
 
-    import Flask  # noqa: F401
+try:
+    import flask  # noqa: F401
+    import pyserial
 except ImportError:
     # If installation failed, exit with 100 to tell the gateway not to restart
     # this process.
