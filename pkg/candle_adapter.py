353fc3d8794464760746c35cac976482fde7c00b
Michael Stegeman <michael@stegeman.me>
2020-03-23 09:05:56 -0800

    Support more architectures.

--- pkg/candle_adapter.py
+++ pkg/candle_adapter.py
@@ -99,7 +99,24 @@ class CandleAdapter(Adapter):
         
         self.add_on_path = os.path.join(os.path.expanduser('~'), '.mozilla-iot', 'addons','Candle-manager-addon')
         print("self.add_on_path = " + str(self.add_on_path))
-        self.arduino_cli_path = os.path.join(self.add_on_path, str(sys.platform))
+
+	if sys.platform == 'darwin':
+	    self.arduino_cli_path = os.path.join(self.add_on_path, 'arduino-cli', 'darwin-x64')
+	elif sys.platform == 'linux':
+	    machine = os.uname().machine
+
+	    if machine == 'aarch64':
+		self.arduino_cli_path = os.path.join(self.add_on_path, 'arduino-cli', 'linux-arm64')
+	    elif machine.startswith('arm'):
+		self.arduino_cli_path = os.path.join(self.add_on_path, 'arduino-cli', 'linux-arm')
+	    elif machine == 'x86_64':
+		self.arduino_cli_path = os.path.join(self.add_on_path, 'arduino-cli', 'linux-x64')
+	    else:
+		self.arduino_cli_path = os.path.join(self.add_on_path, 'arduino-cli', 'linux-ia32')
+	else:
+	    print('Unknown platform!')
+	    self.arduino_cli_path = None
+
         print("self.arduino_cli_path = " + str(self.arduino_cli_path))
         
         self.DEBUG = True
