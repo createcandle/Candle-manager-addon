bee61081a3fe173ce253c2277e77b4ecc971ef4b
Michael Stegeman <michael@stegeman.me>
2020-07-24 11:29:17 -0800

    Clean up packaging and drop package.json.

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-version=$(grep version package.json | cut -d: -f2 | cut -d\" -f2)
+version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
 
 if [ -z "${ADDON_ARCH}" ]; then
   TARFILE_SUFFIX=
@@ -21,33 +21,28 @@ cp source/Candle_cleaner/Candle_cleaner.ino package/code/Candle_cleaner/
 pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
 
 # Put package together
-cp *.py manifest.json package.json LICENSE README.md boards.txt package/
+cp *.py manifest.json LICENSE README.md boards.txt package/
 cp -r lib pkg arduino-cli css images js views package/
 find package -type f -name '*.pyc' -delete
+find package -type f -name '._*' -delete
 find package -type d -empty -delete
 
 cd package
-
-python3 -c "import json, os; \
-    fname = os.path.join(os.getcwd(), 'package.json'); \
-    d = json.loads(open(fname).read()); \
-    d['files'] = list(filter(lambda x: not x.startswith('arduino-cli/') or x.startswith('arduino-cli/${ADDON_ARCH}/'), d['files'])); \
-    f = open(fname, 'wt'); \
-    json.dump(d, f, indent=2); \
-    f.close()
-"
 
 # keep only the compiled DS binary that we need
 find arduino-cli -mindepth 1 -maxdepth 1 \! -name "${ADDON_ARCH}" -exec rm -rf {} \;
 
 # Generate checksums
+echo "generating checksums"
 find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd -
 
 # Make the tarball
+echo "creating archive"
 TARFILE="Candle-manager-addon-${version}${TARFILE_SUFFIX}.tgz"
 tar czf ${TARFILE} package
 
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+cat ${TARFILE}.sha256sum
 
 rm -rf SHA256SUMS package
