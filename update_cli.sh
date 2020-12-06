bc3b4e3d38209a91cbc676e2912bcf8ba73e78c4
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2020-12-06 17:09:48 +0100

    1.1.3
    - Fixes for Webthings 1.0
    - Everything now uses os.path.join
    - Created arduino cli update shell script
    - Updated arduino cli binaries

---
+++ update_cli.sh
@@ -0,0 +1,4 @@
+curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARMv7.tar.gz | tar xzv --directory ./arduino-cli/linux-arm
+curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARM64.tar.gz | tar xzv --directory ./arduino-cli/linux-arm64
+curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz | tar xzv --directory ./arduino-cli/linux-x64
+curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_macOS_64bit.tar.gz | tar xzv --directory ./arduino-cli/darwin-x64
