fa990e97eaa34e5ee841ddca541bb08139d1133a
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2020-12-06 19:01:29 +0100

    Add files via upload

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
 [ $(id -u) = 0 ] && umask 0
 
 # Clean up from previous releases
-rm -rf *.tgz *.sha256sum package SHA256SUMS lib
+rm -rf *.tgz *.pyc ._* *.sha256sum package SHA256SUMS lib
 
 if [ -z "${ADDON_ARCH}" ]; then
   TARFILE_SUFFIX=
@@ -16,32 +16,37 @@ else
 fi
 
 
-
-# Prep new package
-mkdir lib package
+# Put package together
+mkdir -p lib package/source/Candle_cleaner package/code/Candle_cleaner
+cp source/Candle_cleaner/Candle_cleaner.ino package/source/Candle_cleaner/
+cp source/Candle_cleaner/Candle_cleaner.ino package/code/Candle_cleaner/
 
 # Pull down Python dependencies
 pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
 
 # Put package together
-cp -r lib pkg LICENSE manifest.json *.py README.md package/
+cp *.py manifest.json LICENSE README.md boards.txt package/
+cp -r lib pkg arduino-cli css images js views package/
 find package -type f -name '*.pyc' -delete
 find package -type f -name '._*' -delete
 find package -type d -empty -delete
-rm -rf package/pkg/pycache
+
+cd package
+
+# keep only the compiled DS binary that we need
+find arduino-cli -mindepth 1 -maxdepth 1 \! -name "${ADDON_ARCH}" -exec rm -rf {} \;
 
 # Generate checksums
 echo "generating checksums"
-cd package
 find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd -
 
 # Make the tarball
 echo "creating archive"
-TARFILE="mysensors-adapter-${version}${TARFILE_SUFFIX}.tgz"
+TARFILE="Candle-manager-addon-${version}${TARFILE_SUFFIX}.tgz"
 tar czf ${TARFILE} package
 
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
 cat ${TARFILE}.sha256sum
-#sha256sum ${TARFILE}
+
 #rm -rf SHA256SUMS package
