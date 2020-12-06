412119c84f9c2fac906deeb99cef68daf594bb71
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2020-12-06 16:28:12 +0100

    1.1.3

--- package.sh
+++ package.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
 
+version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
+
 # Setup environment for building inside Dockerized toolchain
 [ $(id -u) = 0 ] && umask 0
 
-version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
+# Clean up from previous releases
+rm -rf *.tgz *.sha256sum package SHA256SUMS lib
 
 if [ -z "${ADDON_ARCH}" ]; then
   TARFILE_SUFFIX=
@@ -12,40 +15,33 @@ else
   TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
 fi
 
-# Clean up from previous releases
-rm -rf *.tgz package SHA256SUMS lib
 
-# Put package together
-mkdir -p lib package/source/Candle_cleaner package/code/Candle_cleaner
-cp source/Candle_cleaner/Candle_cleaner.ino package/source/Candle_cleaner/
-cp source/Candle_cleaner/Candle_cleaner.ino package/code/Candle_cleaner/
+
+# Prep new package
+mkdir lib package
 
 # Pull down Python dependencies
 pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
 
 # Put package together
-cp *.py manifest.json LICENSE README.md boards.txt package/
-cp -r lib pkg arduino-cli css images js views package/
+cp -r lib pkg LICENSE manifest.json *.py README.md package/
 find package -type f -name '*.pyc' -delete
 find package -type f -name '._*' -delete
 find package -type d -empty -delete
-
-cd package
-
-# keep only the compiled DS binary that we need
-find arduino-cli -mindepth 1 -maxdepth 1 \! -name "${ADDON_ARCH}" -exec rm -rf {} \;
+rm -rf package/pkg/pycache
 
 # Generate checksums
 echo "generating checksums"
+cd package
 find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd -
 
 # Make the tarball
 echo "creating archive"
-TARFILE="Candle-manager-addon-${version}${TARFILE_SUFFIX}.tgz"
+TARFILE="mysensors-adapter-${version}${TARFILE_SUFFIX}.tgz"
 tar czf ${TARFILE} package
 
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
 cat ${TARFILE}.sha256sum
-
-rm -rf SHA256SUMS package
+#sha256sum ${TARFILE}
+#rm -rf SHA256SUMS package
