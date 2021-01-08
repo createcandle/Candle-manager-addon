750f65d8600191188066529ac33b58ba2f2f0af8
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2021-01-08 14:03:39 +0100

    added armv6 arduino cli

--- update_cli.sh
+++ update_cli.sh
@@ -1,4 +1,5 @@
-curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARMv7.tar.gz | tar xzv --directory ./arduino-cli/linux-arm
+curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARMv7.tar.gz | tar xzv --directory ./arduino-cli/linux-armv7
+curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARMv6.tar.gz | tar xzv --directory ./arduino-cli/linux-armv6
 curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARM64.tar.gz | tar xzv --directory ./arduino-cli/linux-arm64
 curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz | tar xzv --directory ./arduino-cli/linux-x64
 curl -L https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_macOS_64bit.tar.gz | tar xzv --directory ./arduino-cli/darwin-x64
